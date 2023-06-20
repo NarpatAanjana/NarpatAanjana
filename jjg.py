c91062ce39e0c8a266b451cb2c4ec17f86054fb1
Narpat Aanjana <narpatanjana0@gmail.com>
2023-06-20 17:55:52 +0530

    Delete jjg.py

--- jjg.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def middleNode(self, head):
-        end = head
-        while end is not None and end.next is not None:
-            head = head.next
-            end = end.next.next
-        return head
